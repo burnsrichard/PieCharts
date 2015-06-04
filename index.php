c90d75f84ba12b0a8d1ec9ab2ad42d17b66d2611
Richard Burns <rburns@wcupa.edu>
2015-06-04 13:33:46 -0400

    initial skeleton code

---
+++ index.php
@@ -0,0 +1,24 @@
+<?php
+
+require 'credentials.php';
+
+// Create connection
+$conn = new mysqli($servername, $username, $password, $dbname);
+// Check connection
+if ($conn->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
+}
+
+$sql = "SELECT `Index`, `Id`, `Date` FROM `Corpus`";
+$result = $conn->query($sql);
+
+if ($result->num_rows > 0) {
+    // output data of each row
+    while($row = $result->fetch_assoc()) {
+        echo "Index: " . $row["Index"]. " - Id: " . $row["Id"]. " - Date: " . $row["Date"]. "<br>";
+    }
+} else {
+    echo "0 results";
+}
+$conn->close();
+?> 

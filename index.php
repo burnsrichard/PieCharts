f8eab64ba0b9c9cbd9d10c74a324e30c8cf1e92b
Vicki <wdnode@rocketmail.com>
2015-06-18 13:34:40 -0400

    added html table and stylized table

--- index.php
+++ index.php
@@ -2,7 +2,7 @@
 <html lang="en">
 <head>
 	<meta charset="utf-8">
-	<title>Corpus</title>
+	<title>PieCharts</title>
 
 	<link rel="stylesheet" href="css/style.css">
 </head>
@@ -22,27 +22,43 @@ $password = "";
 // username and password re-defined in external php file
 // included in .gitignore, so they're never committed into repo
 require 'credentials.php';
-
+?>
+<table>
+    <tr>
+        <th>Index</th>
+        <th>Id</th>  
+        <th>Date</th>
+        <th>Source</th>
+        <th>Headline</th>
+        <th>Caption</th>  
+    </tr>
+<?php
 try {
 
 	// Create connection
 	$dbh = new PDO("mysql:host=$host;port=$port;dbname=$dbname", $username, $password);
-
-	$sql = "SELECT `Index`, `Id`, `Date`, `Source`, `Headline` FROM `Corpus`";
-    foreach($dbh->query($sql) as $row) {
-        // print_r($row);
-        echo "Index: " . $row["Index"]. " - Id: " . $row["Id"]. " - Date: " . $row["Date"]. " - Source: ". $row["Source"].
-        	" - Headline: ". $row["Headline"]. "<br>";
-    }
-
 } catch (PDOException $e) {
     print "Error!: " . $e->getMessage() . "<br/>";
     die();
 }
-
+	$sql = "SELECT `Index`, `Id`, `Date`, `Source`, `Headline`, `Caption` FROM `Corpus`";
+    foreach($dbh->query($sql) as $row) {
+    ?>
+        <tr>
+            <td><?php echo $row["Index"]?></td>
+            <td><?php echo $row["Id"]?></td> 
+            <td><?php echo $row["Date"]?></td>
+            <td><?php echo $row["Source"]?></td>
+        	<td><?php echo $row["Headline"]?></td>
+            <td><?php echo $row["Caption"]?></td>
+        </tr>
+    <?php
+    }
+    ?>
+</table>
 
 // close the connection
 $dbh = null;
-?> 
+?>
 </body>
 </html>

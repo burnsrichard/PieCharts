2df1513298ed78fd5462f6e5694bc88f0a8a801a
Domanowska <domanowskaw@gmail.com>
2015-09-01 21:04:33 -0400

    created header.php to include header on each page

---
+++ header.php
@@ -0,0 +1,5 @@
+<div id="header">Pie Charts 
+						<br> <a href="index.php">Corpus</a> | 
+						<a href="groupedpies.php">Grouped Pie Charts</a> |
+        				<a href="messages.php">Message Categories</a>
+		</div>
